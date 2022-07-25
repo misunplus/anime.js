94b210b97e3fe8629350fcdda8b9d43acd8b7885
Beckia <63723700+misunplus@users.noreply.github.com>
2022-07-25 23:59:58 +0900

    로딩 텍스트 애니메이션

--- anime.js
+++ anime.js
@@ -1,17 +1,70 @@
-var logEl = document.querySelector('.battery-log');
-  
-var battery = {
-    글씨: '0',
-    퍼센트: 5
-}
+const textFirst = document.querySelector('.text.first');
+const textSecond = document.querySelector('.text.second');
+textFirst.innerHTML = textFirst.textContent.replace(/\S/g, "<span>$&</span>")
+textSecond.innerHTML = textSecond.textContent.replace(/\S/g, "<span>$&</span>")
 
-anime({
-    targets: battery,
-    글씨: '100',
-    퍼센트: 1000,
-    round: 1,
-    easing: 'linear',
-    update: function () {
-        logEl.innerHTML = JSON.stringify(battery);
+anime.timeline(
+    {
+        loop: false
     }
-});
+)
+
+.add(
+    {
+        targets: '.text.first span',
+        translateY: [-600, 0],
+        scale: [10, 1],
+        opacity: [0, 1],
+        easing: "easeOutExpo",
+        duration: 1500,
+        delay: anime.stagger(100),
+    }
+)
+
+.add(
+    {
+        targets: '.text.first span',
+        translateX: [0, -1000],
+        rotate: [0, -360],
+        scale: [1, 1],
+        opacity: [1, 0],
+        easing: "easeOutExpo",
+        duration: 2000,
+        delay: anime.stagger(100),
+
+    }
+)
+
+.add(
+    {
+        targets: '.text.first span',
+        delay:0,
+        begin: function() {
+            document.querySelector('.text.first').style.display = 'none';
+            document.querySelector('.text.second').style.display = 'block';
+        },
+    }
+)
+.add(
+    {
+        targets: '.text.second span',
+        translateX: [1000, 0],
+        rotate: [360,0],
+        scale: [1, 1],
+        opacity: [0, 1],
+        easing: "easeOutExpo",
+        duration: 2000,
+        delay: anime.stagger(100),
+    }
+)
+.add(
+    {
+        targets: '.text.second span',
+        translateX: [0, 0],
+        scale: [1,50],
+        opacity: [1, 0],
+        easing: "easeOutExpo",
+        duration: 2000,
+        delay: anime.stagger(100),
+    }
+)
